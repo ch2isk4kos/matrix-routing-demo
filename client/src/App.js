be1a8da9544bfb768e6344503cb401a674798135
ch2isk4kos <ch2isk4kos@gmail.com>
2021-06-02 14:32:02 -0400

    import react hooks and create a new tomtom map inside of useEffect to instantiate on render

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,17 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 const App = () => {
+  const [map, setMap] = useState({});
+
+  useEffect(() => {
+    let map = tt.map({
+      key: process.env.REACT_APP_TT_KEY,
+      container: mapElement.current,
+    });
+    setMap(map);
+  }, []);
+
   return (
     <div className="App">
       <h1>Welcome Home</h1>
